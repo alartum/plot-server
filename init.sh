cad58874115259cb5ba644bd8cd30f1bb2c112c0
Aleksandr Artemenkov <alartum@gmail.com>
2018-12-05 22:39:20 +0300

    Refixed typo

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Installing packages
-pip3 install --upgrade pip3
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 nodeenv -p
 npm install -g chart.js --save
